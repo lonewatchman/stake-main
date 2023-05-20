088e2f2c60ae3b5d60a35af19fff94eb074830f5
Terra-Credit <lonewatchmanofficial@gmail.com>
2023-05-20 11:27:09 +0200

    1

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -450,7 +450,7 @@ function Home() {
                                         <th>Unstake Date</th>
                                         <th>Earn Reward</th>
                                         <th>Unstake</th>
-                                        <th>Harvest</th>
+                                        <th>Unavailable</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -463,6 +463,7 @@ function Home() {
                                                     <td>{rowdata[0] ? new Date(rowdata[0] * 1000).toGMTString() : ' - '}</td>
                                                     <td>{accStats.rewardHistory[index] ? parseFloat(accStats.rewardHistory[index][0] / Math.pow(10, accStats.rewardTokenDecimals)).toFixed(5) : 0}</td>
                                                     <td><Button onClick={() => handleUnStake(index)} disabled={rowdata[10]} className="theme-btn">Unstake</Button></td>
+                                                    <td><Button onClick={() => handleHarvest(index)} disabled={rowdata[10]} className="theme-btn">Unavailable</Button></td>
                                                 </tr>
                                             )
                                         })
